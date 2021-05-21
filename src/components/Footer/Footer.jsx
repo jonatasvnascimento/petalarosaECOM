9c61aad37a4470820fce2996cdf687e88148f042
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-05-21 14:50:03 -0300

    Initial Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-center text-white">
+      <div className="container p-4">
+        <section className="mb-4">
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-facebook-f"></i
+          ></a>
+
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-twitter"></i
+          ></a>
+
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-google"></i
+          ></a>
+
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-instagram"></i
+          ></a>
+
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-linkedin-in"></i
+          ></a>
+
+          <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+          ><i className="fab fa-github"></i
+          ></a>
+        </section>
+
+        <section className="">
+          <form action="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-auto">
+                <p className="pt-2">
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </div>
+            </div>
+          </form>
+        </section>
+
+        <section className="mb-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+            repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+            eum harum corrupti dicta, aliquam sequi voluptate quas.
+      </p>
+        </section>
+        <section className="">
+          <div className="row">
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Links</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Links</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Links</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">Links</h5>
+
+              <ul className="list-unstyled mb-0">
+                <li>
+                  <a href="#!" className="text-white">Link 1</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 2</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 3</a>
+                </li>
+                <li>
+                  <a href="#!" className="text-white">Link 4</a>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </section>
+
+        <div className="text-center p-3 style_footer not_style">
+          © 2020 Copyright:
+          <a className="text-white " href="##"> GhostBit</a>
+        </div>
+
+      </div>
+    </footer>
+  )
+}
